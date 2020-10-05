22e00b9e49bf307fab0e9a34e7d592ef189617ae
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-05 20:15:38 +0300

    create Blog Pages programmatically

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,3 +1,4 @@
+
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
@@ -7,13 +8,12 @@ const ComponentName = () => {
   return <h2>blog post template</h2>
 }
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
-
-//     }
-//   }
-// `
+export const query = graphql`
+  query GetSingleBlog($slug: String) {
+    blog: strapiBlogs(slug: { eq: $slug }) {
+      content
+    }
+  }
+`
 
 export default ComponentName
