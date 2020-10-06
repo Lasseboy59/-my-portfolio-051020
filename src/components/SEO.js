3c3f2c66e5b29e1b5882ba204ddd3bef86fb65f0
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-06 17:31:00 +0300

    added SEO query

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,9 +2,29 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        siteDesc: description
+        author
+        twitterUsername
+        image
+        siteTitle: title
+        siteUrl
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
-  return <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-    <meta name="description" conent={description} />
+  const { site } = useStaticQuery(query);
+  const { siteDesc, siteTitle, siteUrl, image, twitterUsername } = site.siteMetadata
+
+  return <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
+    <meta name="description" conent={description || siteDesc} />
+    <meta name="image" content={image} />
   </Helmet>
 }
 
